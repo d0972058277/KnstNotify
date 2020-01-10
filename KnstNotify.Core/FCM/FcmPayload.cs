4e6bf12ba0216338d738315ba370df40190bd170
PorriDG3 <gn02053231@gmail.com>
2020-01-10 09:31:11 +0800

    modify

--- KnstNotify.Core/FCM/FcmPayload.cs
+++ KnstNotify.Core/FCM/FcmPayload.cs
@@ -10,26 +10,37 @@ namespace KnstNotify.Core.FCM
     {
         [JsonPropertyName("to")]
         public string To { get; set; }
+
         [JsonPropertyName("registration_ids")]
         public IEnumerable<string> RegistrationIds { get; set; }
+
         [JsonPropertyName("condition")]
         public string Condition { get; set; }
+
         [JsonPropertyName("collapse_key")]
         public string CollapseKey { get; set; }
+
         [JsonPropertyName("priority")]
         public string Priority { get; set; } = "high";
+
         [JsonPropertyName("content_available")]
         public bool? ContentAvailable { get; set; }
+
         [JsonPropertyName("mutable_content")]
         public bool? MutableContent { get; set; }
+
         [JsonPropertyName("time_to_live")]
         public decimal? TimeToLive { get; set; }
+
         [JsonPropertyName("restricted_package_name")]
         public string RestrictedPackageName { get; set; }
+
         [JsonPropertyName("dry_run")]
         public bool? DryRun { get; set; }
+
         [JsonPropertyName("data")]
         public IDictionary<string, object> Data { get; set; }
+
         [JsonPropertyName("notification")]
         public IDictionary<string, object> Notification { get; set; }
     }
